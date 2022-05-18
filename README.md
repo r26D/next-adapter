2abf038cbc708876a3d3bd3bd6795250c04c6774
Dirk Elmendorf <dirk@r26d.com>
2022-05-18 15:04:29 -0500

    Modified to support babel transpiling of the javascript

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Adapter document and server for Next.js projects using Expo modules.
 
 This version updates NextJS and strips out the need for typescript when you just checkout
 the package - since it isn't being shipped via npm
+
+
+## To Update
+
+1. Sync the project code into the ts_version
+1. Update the package deps in package.json
+1. cd tools && npm start
+1. commit and push
--- tools/convert.js
+++ tools/convert.js
@@ -3,21 +3,21 @@ const utils = require('./utils');
 const fse = require('fs-extra');
 
 Promise.all([
-    fse.copy('../ts_version/babel', '../dist/babel'),
-    fse.copy('../ts_version/src', '../dist/src'),
-    fse.copy('../ts_version/customize', '../dist/customize'),
-    fse.copy('../ts_version/template', '../dist/template'),
-    fse.copy('../ts_version/.eslintrc.js', '../dist/.eslintrc.js'),
-    fse.copy('../.gitignore', '../dist/.gitignore'),
-    fse.copy('../ts_version/document.js', '../dist/document.js'),
-    fse.copy('../ts_version/LICENSE', '../dist/LICENSE'),
- //   fse.copy('../ts_version/package.json', '../dist/package.json'),
-    fse.copy('../ts_version/README.md', '../dist/README.md'),
+    fse.copy('../ts_version/babel', '../js_version/babel'),
+    fse.copy('../ts_version/src', '../js_version/src'),
+    fse.copy('../ts_version/customize', '../js_version/customize'),
+    fse.copy('../ts_version/template', '../js_version/template'),
+    fse.copy('../ts_version/.eslintrc.js', '../js_version/.eslintrc.js'),
+    fse.copy('../.gitignore', '../js_version/.gitignore'),
+    fse.copy('../ts_version/document.js', '../js_version/document.js'),
+    fse.copy('../ts_version/LICENSE', '../js_version/LICENSE'),
+ //   fse.copy('../ts_version/package.json', '../js_version/package.json'),
+    fse.copy('../ts_version/README.md', '../js_version/README.md'),
 ])
     .then(() => {
-        utils.createJsConfig('../dist/jsconfig.json');
+        utils.createJsConfig('../js_version/jsconfig.json');
 
-        const allFiles = utils.buildTree('../dist/src');
+        const allFiles = utils.buildTree('../js_version/src');
         utils.transformTsToJs(allFiles);
     })
     .catch((err) => console.log(err));
