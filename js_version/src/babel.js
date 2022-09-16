8a0b96ddaa282fbf6465d93a63669ec44abc8ba6
Dirk Elmendorf <dirk@r26d.com>
2022-09-16 16:54:03 -0500

    Forcing over to automatic.

--- js_version/src/babel.js
+++ js_version/src/babel.js
@@ -17,11 +17,14 @@ module.exports = function (api) {
     presets: [// Only use next in the browser, it'll break your native project
     isWeb && require('next/babel'), [require('babel-preset-expo'), {
       web: {
-        useTransformReactJSXExperimental: true
+       // useTransformReactJSXExperimental: true,
+
       },
       // Disable the `no-anonymous-default-export` plugin in babel-preset-expo
       // so users don't see duplicate warnings.
-      'no-anonymous-default-export': false
+      'no-anonymous-default-export': false,
+        jsxRuntime: 'automatic',
+        jsxImportSource: 'react'
     }]].filter(Boolean)
   };
 };
